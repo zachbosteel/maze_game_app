6800036ed82d97d7f328dcee9687b910aad617fb
Zach Bosteel <zbosteel@gmail.com>
2015-07-08 19:18:32 -0500

    basic camera functionality implemented

--- app/assets/javascripts/games/game_window.js
+++ app/assets/javascripts/games/game_window.js
@@ -4,14 +4,47 @@
         actionService.init(window);
         var stage = new createjs.Stage("main");
 
-        var hero1 = new Hero('Hero 1', 50, 50);
-        stage.addChild(hero1);
+        var world = new createjs.Container();
+        world.x = 0
+        world.y = 0
+        stage.addChild(world)
 
-        console.log(hero1)
+        var hero = new Hero('Hero 1', 50, 50);
+        world.addChild(hero);
+
+        var _W = 500
+          , _H = 700
+          , xCentre = _W / 2
+          , yCentre = _H / 2
+          , wWidth = 2000
+          , wHeight = 2000;
+
+        console.log(hero)
 
         createjs.Ticker.timingMode = createjs.Ticker.RAF;
         createjs.Ticker.setFPS(60);
         createjs.Ticker.addEventListener("tick", function (event) { 
           stage.update();
+          cameraMove();
         });
+
+        function cameraMove(){
+          if (wWidth > _W) {
+            if (hero.x < wWidth - xCentre && hero.x > xCentre)
+              world.x = -hero.x + xCentre;
+            else if (hero.x >= wWidth - xCentre)
+              world.x = -(wWidth - _W);
+            else
+              world.x = 0;
+          }
+
+          if (wHeight > _H) {
+            if (hero.y < wHeight - yCentre && hero.y > yCentre)
+              world.y = -hero.y + yCentre;
+            else if (hero.y >= wHeight - yCentre)
+              world.y = -(wHeight - _H);
+            else
+              world.y = 0;
+          }
+        };
       };
